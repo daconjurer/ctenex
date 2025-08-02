8ca24cc217973fa68e8d520150555ee86f813959
Victor Sandoval <47492578+daconjurer@users.noreply.github.com>
2025-08-02 20:48:39 +0100

    feat: Add timestamp filtering for GET /orders (#30)

--- ctenex/core/db/utils.py
+++ ctenex/core/db/utils.py
@@ -1,3 +1,5 @@
+from typing import Type
+
 from sqlalchemy.inspection import inspect
 
 from ctenex.core.db.base import AbstractBase
@@ -5,3 +7,7 @@ from ctenex.core.db.base import AbstractBase
 
 def get_entity_values(entity: AbstractBase) -> dict:
     return {c.key: getattr(entity, c.key) for c in inspect(entity).mapper.column_attrs}
+
+
+def get_entity_fields(entity: Type[AbstractBase]) -> list[str]:
+    return [c.key for c in inspect(entity).mapper.column_attrs]
